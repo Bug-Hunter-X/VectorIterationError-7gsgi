148806a2e44e1d37c541f5194581a8ed11296499
BugHunter-X <askzenai@gmail.com>
2024-12-05 00:40:06 +0100

    Add bug.cpp file

---
+++ bug.cpp
@@ -0,0 +1,13 @@
+```cpp
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::vector<int> vec = {1, 2, 3, 4, 5};
+  for (int i = 0; i <= vec.size(); ++i) { // Error: Accessing vec[vec.size()] is out of bounds
+    std::cout << vec[i] << " ";
+  }
+  std::cout << std::endl;
+  return 0;
+}
+```
